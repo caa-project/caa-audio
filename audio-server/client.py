d477f8076cfcc9ad8054f21671eeaebbed512ae3
Suguru Kato <salmon.bioinformatics@gmail.com>
2014-11-18 19:38:30 +0900

    added gflags to client.py

--- audio-server/client.py
+++ audio-server/client.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import gflags
 import websocket
 import datetime
 import time
@@ -7,56 +8,57 @@ import wave
 import pyaudio
 import sys
 
-if len(sys.argv) < 2:
-    print >> sys.stderr, "Require 2 parameters: server_address and index"
-    sys.exit(1)
+gflags.DEFINE_float("delay", 0.1, "the initial delay")
+gflags.DEFINE_string("server", None, "the server address")
 
-host = sys.argv[1]
-index = sys.argv[2]
+def main(argv):
+    argv = gflags.FLAGS(argv)
 
-FORMAT = pyaudio.paInt16
-#FORMAT = pyaudio.paFloat32
-RATE = 44100
+    FORMAT = pyaudio.paInt16
+    RATE = 44100
 
-p = pyaudio.PyAudio()
-stream = p.open(
-        format=FORMAT,
-        channels=1,
-        rate=RATE,
-        output=True)
+    p = pyaudio.PyAudio()
+    stream = p.open(
+            format=FORMAT,
+            channels=1,
+            rate=RATE,
+            output=True)
 
-initial_delay_sec = datetime.timedelta(seconds=2.0);
+    initial_delay_sec = datetime.timedelta(seconds=gflags.FLAGS.delay);
 
-def on_message(ws, message):
-    current_time = datetime.datetime.now()
-    duration = datetime.timedelta(seconds=1.0/RATE*len(message)/2)
-    if current_time < ws.scheduled_time:
-        time.sleep((ws.scheduled_time - current_time).total_seconds())
-        stream.write(message)
-        ws.scheduled_time += duration
-    else:
-        stream.write(message)
-        ws.scheduled_time = current_time + duration + initial_delay_sec
+    def on_message(ws, message):
+        current_time = datetime.datetime.now()
+        duration = datetime.timedelta(seconds=1.0/RATE*len(message)/2)
+        if current_time < ws.scheduled_time:
+            time.sleep((ws.scheduled_time - current_time).total_seconds())
+            stream.write(message)
+            ws.scheduled_time += duration
+        else:
+            stream.write(message)
+            ws.scheduled_time = current_time + duration + initial_delay_sec
 
-def on_close(ws):
-    print '[on close]'
+    def on_close(ws):
+        print '[on close]'
 
-def on_open(ws):
-    print '[on open]'
+    def on_open(ws):
+        print '[on open]'
 
-while True:
-    try:
-        ws = websocket.WebSocketApp('ws://%s/user/%s' % (host, index),
-                on_message=on_message,
-                on_close=on_close)
-        ws.scheduled_time = datetime.datetime.now() + initial_delay_sec
-        ws.on_open = on_open
-        ws.run_forever()
-        time.sleep(1)
-    except KeyboardInterrupt:
-        break
+    while True:
+        try:
+            ws = websocket.WebSocketApp(gflags.FLAGS.server,
+                    on_message=on_message,
+                    on_close=on_close)
+            ws.scheduled_time = datetime.datetime.now() + initial_delay_sec
+            ws.on_open = on_open
+            ws.run_forever()
+            time.sleep(1)
+        except KeyboardInterrupt:
+            break
 
-ws.close()
-stream.stop_stream()
-stream.close()
-p.terminate()
+    ws.close()
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+if __name__ == '__main__':
+    main(sys.argv)
